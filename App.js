0228d98dcd1e2d743214b201849996084533c8e6
Senbonsakurakageyoshi1 <699580621meliga@gmail.com>
2023-09-19 18:31:34 +0100

    Setup  of the map and the markers

--- App.js
+++ App.js
@@ -1,11 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import MapView,{Marker} from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import MapViewDirections from 'react-native-maps-directions';
 
 export default function App() {
+
+  const [state,setState] = useState({
+    pickupCords:{
+      latitude:3.834027,
+      longitude: 11.506229,
+      latitudeDelta:0.0922,longitudeDelta:0.021
+    },
+    droplocationCords:{
+      latitude: 3.824874, 
+      longitude:11.504672,
+      latitudeDelta:0.0922,longitudeDelta:0.021
+    }
+  })
+  const mapRef = useRef()
+  const {pickupCords,droplocationCords} = state
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView  ref={mapRef} style={styles.map}  initialRegion={pickupCords}>
+
+
+ <Marker coordinate={pickupCords}/>
+<Marker coordinate={droplocationCords}/> 
+
+
+<MapViewDirections
+            origin={pickupCords}
+            
+            destination={droplocationCords}
+            apikey="AIzaSyCmZ8NWN3DsTtIJ-BDDwZfimvZz2f2xpE8"
+            strokeWidth={3}
+            strokeColor='hotpink'
+            optimizeWaypoints={true}
+            onReady={result => {mapRef.current.fitToCoordinates(result.coordinates,{edgePadding:{
+              right:30,bottom:300,left:30,top:30
+            }})}}
+          
+            
+          />
+
+</MapView>
     </View>
   );
 }
@@ -13,8 +51,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
