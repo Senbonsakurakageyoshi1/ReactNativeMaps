d73247c12e5778bae1da9c127c2fe5a1ce413c68
Senbonsakurakageyoshi1 <699580621meliga@gmail.com>
2023-10-02 18:14:10 +0100

    02/12/2023 18:13"

--- src/Screens/ChooseLocation.js
+++ src/Screens/ChooseLocation.js
@@ -1,74 +1,75 @@
-import { StyleSheet, Text, View ,ScrollView} from 'react-native'
-import React, { useState } from 'react'
-import AdressPickup from '../components/AdressPickup';
+//import libraries
 import { useNavigation } from '@react-navigation/native';
+import React, { Component, useState } from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 
-import CustomBtn from '../components/CustomBtn'
-import {showError,showSuccess} from '../helper/helperFunction'
+//reusable components
+import AddressPickup from '../components/AdressPickup';
+import CustomBtn from '../components/CustomBtn';
+import { showError } from '../helper/helperFunction';
 
 const ChooseLocation = (props) => {
+    const navigation = useNavigation()
 
-  const navigation = useNavigation()
+    const [state, setState] = useState({
+        destinationCords: {}
+    })
 
-  const [state,setState]=useState({//pickupCords:{},
-    destinationCords:{}})
+    const { destinationCords } = state
 
-  const {//pickupCords,
-    destinationCords}=state
-
-  const checkValid=()=>{
-   
-
-    if(Object.keys(destinationCords).length==0){
-      showError('Please  enter your destination Location')
-      return false
-    }
-    return true
-  }
-
-
-  const onDone=()=>{
-    const isvalid=checkValid()
-
-    if(isvalid){
-      props.route.params.getCordinates({
-
-        //pickupCords,
-        destinationCords
-      })
-      showSuccess('Ginkaku')
-      navigation.goBack()
-
-
+    const checkValid = () =>{
+        if(Object.keys(destinationCords).length === 0){
+            showError('Please enter your destination location')
+            return false
+        }
+        return true
     }
 
-  }
-  // const fetchAddressCords=(lat,lng)=>{
-  //   setState({...state,pickupCords:{latitude:lat,longitude:lng}})
+    const onDone = () => {
+        const isValid = checkValid()
+        if(isValid){
+            props.route.params.getCordinates({
+                destinationCords
+            })
+            navigation.goBack()
+        }
+    }
+    const fetchDestinationCords = (lat, lng, zipCode, cityText) => {
+        console.log("zip code==>>>",zipCode)
+        console.log('city texts',cityText)
+        setState({
+            ...state,
+            destinationCords: {
+                latitude: lat,
+                longitude: lng
+            }
+        })
+    }
 
-  // }
-
-  const fetchDestinationCords=(lat,lng)=>{
-    setState({...state,destinationCords:{latitude:lat,longitude:lng}})
-
-
-  }
-  return (
-    <View style={styles.container}>
-      <ScrollView  keyboardShouldPersistTaps="handled" style={{backgroundColor:'white',flex:1,padding:24}}>
-      <View style={{marginBottom:16}}/>
-      <AdressPickup  placeholderText="Enter destination Location"   fetchAddress={fetchDestinationCords} />
-      <CustomBtn btnText='Done' btnStyle={{marginTop:24}} onPress={onDone}  />
-      </ScrollView>
-    </View>
-  )
-}
-
-export default ChooseLocation
-
+    return (
+        <View style={styles.container}>
+            <ScrollView
+                keyboardShouldPersistTaps="handled"
+                style={{ backgroundColor: 'white', flex: 1, padding: 24 }}
+            >
+                <View style={{ marginBottom: 16 }} />
+                <AddressPickup
+                    placheholderText="Enter Destination Location"
+                    fetchAddress={fetchDestinationCords}
+                />
+                <CustomBtn
+                    btnText="Done"
+                    onPress={onDone}
+                    btnStyle={{marginTop: 24}}
+                />
+            </ScrollView>
+        </View>
+    );
+};
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
+    container: {
+        flex: 1,
 
-    }
+    },
 });
+export default ChooseLocation;
